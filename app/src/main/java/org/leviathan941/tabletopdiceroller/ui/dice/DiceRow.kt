4577e8e9520084997c36e4aea64e82fa7acb56fd
Alexey Kuzin <amkuzink@gmail.com>
2021-10-14 21:51:10 +0300

    Show arrow when dice row can be scrollable

--- app/src/main/java/org/leviathan941/tabletopdiceroller/ui/dice/DiceRow.kt
+++ app/src/main/java/org/leviathan941/tabletopdiceroller/ui/dice/DiceRow.kt
@@ -19,12 +19,27 @@
 package org.leviathan941.tabletopdiceroller.ui.dice
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import org.leviathan941.tabletopdiceroller.R
 import org.leviathan941.tabletopdiceroller.viewmodel.DiceRowViewModel
 
 @Composable
@@ -33,16 +48,67 @@ fun DiceRow(
     rowModel: DiceRowViewModel
 ) {
     val diceModels = rowModel.diceModels
-    LazyRow(
-        horizontalArrangement = Arrangement.Center,
-        modifier = modifier
-            .padding(horizontal = DICE_PADDING_ALL_DP),
+    val rowState = rememberLazyListState()
+    var rowSize by remember { mutableStateOf(IntSize.Zero) }
+    Box(
+        modifier = Modifier.fillMaxWidth()
     ) {
-        items(diceModels) { diceModel ->
-            DiceView(diceViewModel = diceModel) { rowModel.removeDice(diceModel) }
+
+        LazyRow(
+            state = rowState,
+            horizontalArrangement = Arrangement.Center,
+            modifier = modifier
+                .align(Alignment.Center)
+                .onSizeChanged { rowSize = it },
+        ) {
+            items(diceModels) { diceModel ->
+                DiceView(diceViewModel = diceModel) { rowModel.removeDice(diceModel) }
+            }
+            if (diceModels.size < MAX_DICE_ROW_SIZE) {
+                item { DiceAddPlaceholder(rowModel::addDice) }
+            }
         }
-        if (diceModels.size < MAX_DICE_ROW_SIZE) {
-            item { DiceAddPlaceholder(rowModel::addDice) }
+
+        Header(Modifier.align(Alignment.CenterStart), rowState)
+
+        Footer(Modifier.align(Alignment.CenterEnd), rowState, rowSize)
+    }
+}
+
+@Composable
+private fun Header(
+    modifier: Modifier,
+    rowState: LazyListState,
+) {
+    if (rowState.firstVisibleItemIndex > 0 ||
+        rowState.firstVisibleItemScrollOffset > 0) {
+        Icon(
+            imageVector = Icons.Filled.KeyboardArrowLeft,
+            contentDescription = stringResource(id = R.string.dice_row_header_content_desc),
+            modifier = modifier.size(40.dp),
+            tint = colorResource(id = R.color.brown_200),
+        )
+    }
+}
+
+@Composable
+private fun Footer(
+    modifier: Modifier,
+    rowState: LazyListState,
+    rowSize: IntSize
+) {
+    with(rowState.layoutInfo) {
+        visibleItemsInfo.lastOrNull()?.let { lastItem ->
+            if (lastItem.index < totalItemsCount - 1 ||
+                lastItem.offset + lastItem.size > rowSize.width
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.KeyboardArrowRight,
+                    contentDescription = stringResource(id = R.string.dice_row_footer_content_desc),
+                    modifier = modifier.size(40.dp),
+                    tint = colorResource(id = R.color.brown_200),
+                )
+            }
         }
     }
 }
