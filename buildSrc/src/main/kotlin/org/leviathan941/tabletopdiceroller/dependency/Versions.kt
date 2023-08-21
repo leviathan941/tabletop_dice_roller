4bdcdb1b8f4a506d3eea9e224cc6e6943948c254
Alexey Kuzin <amkuzink@gmail.com>
2023-08-21 20:26:45 +0300

    Update gradle and AGP
    * Gradle to 8.2.1
    * AGP to 8.1.0

--- buildSrc/src/main/kotlin/org/leviathan941/tabletopdiceroller/dependency/Versions.kt
+++ buildSrc/src/main/kotlin/org/leviathan941/tabletopdiceroller/dependency/Versions.kt
@@ -24,7 +24,7 @@ object Versions {
     const val ACCOMPANIST = "0.30.1"
     const val ACTIVITY_COMPOSE = "1.7.2"
     const val ANDROID_CORE = "1.10.1"
-    const val ANDROID_PLUGIN = "8.0.2"
+    const val ANDROID_PLUGIN = "8.1.0"
     const val APP_COMPAT = "1.6.1"
     const val COMPOSE = "1.4.3"
     const val COMPOSE_COMPILER = "1.4.7"
